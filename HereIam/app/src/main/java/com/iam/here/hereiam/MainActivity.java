9d93f507a84566d6c77de9b04f8a30919a7e5e64
Eliezer de Jesus Oliveira <eliezerdejj@gmail.com>
2017-06-17 14:11:47 -0300

    Projeto - Criação
    Criação do projeto (Android Studio) - Here I am

---
+++ HereIam/app/src/main/java/com/iam/here/hereiam/MainActivity.java
@@ -0,0 +1,52 @@
+package com.iam.here.hereiam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Activity Lifecycle - Clico de vida da aplicação
+     * Cria a aplicação para ser visivel ao usuário
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     *Inicia a aplicação se a mesma já foi criada
+     */
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    /**
+     * O onStop() é chamado quando a Activity não está mais visível
+     */
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    /**
+     * é chamado depois que uma Activity foi interrompida
+     */
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    /**
+     * o onDestroy() é chamado por dois motivos, primeiro se você solicitou a finalização da Activity utilizando o método finish() O
+     * Ou se o sistema operacional destruiu a instância Activity para economizar espaço
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}

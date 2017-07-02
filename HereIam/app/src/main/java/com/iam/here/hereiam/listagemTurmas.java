1a6400e920ad1370d1f9dd5c0423ee5fa9472e04
Eliezer de Jesus Oliveira <eliezerdejj@gmail.com>
2017-07-02 15:12:05 -0300

    Correções em listagem de Turmas

--- HereIam/app/src/main/java/com/iam/here/hereiam/listagemTurmas.java
+++ HereIam/app/src/main/java/com/iam/here/hereiam/listagemTurmas.java
@@ -63,4 +63,38 @@ public class listagemTurmas extends ListActivity {
 
         super.onResume();
     }
+
+    @Override
+    protected void onStart(){
+
+        super.onStart();
+    }
+
+    /**
+     * O onStop() é chamado quando a Activity não está mais visível
+     */
+    @Override
+    protected void onStop() {
+
+        super.onStop();
+    }
+
+    /**
+     * é chamado depois que uma Activity foi interrompida
+     */
+    @Override
+    protected void onRestart() {
+
+        super.onRestart();
+    }
+
+    /**
+     * o onDestroy() é chamado por dois motivos, primeiro se você solicitou a finalização da Activity utilizando o método finish() O
+     * Ou se o sistema operacional destruiu a instância Activity para economizar espaço
+     */
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+    }
 }

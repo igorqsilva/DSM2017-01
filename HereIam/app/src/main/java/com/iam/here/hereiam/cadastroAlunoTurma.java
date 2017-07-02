5c1494c8835fa48fe71f60cb330b9db9ce88eddb
Eliezer de Jesus Oliveira <eliezerdejj@gmail.com>
2017-07-02 15:11:48 -0300

    Correção cadastro de Turmas

--- HereIam/app/src/main/java/com/iam/here/hereiam/cadastroAlunoTurma.java
+++ HereIam/app/src/main/java/com/iam/here/hereiam/cadastroAlunoTurma.java
@@ -18,4 +18,38 @@ public class cadastroAlunoTurma extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_aluno_turma);
     }
+
+    @Override
+    protected void onStart(){
+
+        super.onStart();
+    }
+
+    /**
+     * O onStop() é chamado quando a Activity não está mais visível
+     */
+    @Override
+    protected void onStop() {
+
+        super.onStop();
+    }
+
+    /**
+     * é chamado depois que uma Activity foi interrompida
+     */
+    @Override
+    protected void onRestart() {
+
+        super.onRestart();
+    }
+
+    /**
+     * o onDestroy() é chamado por dois motivos, primeiro se você solicitou a finalização da Activity utilizando o método finish() O
+     * Ou se o sistema operacional destruiu a instância Activity para economizar espaço
+     */
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+    }
 }

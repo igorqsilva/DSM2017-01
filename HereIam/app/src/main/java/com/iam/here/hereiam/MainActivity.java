69790ffb24ee373798a35b2e9a4eb36f39df27e8
Igor Q. Silva <igor-qs@hotmail.com>
2017-07-03 19:15:44 -0300

    Update login e cadastro

--- HereIam/app/src/main/java/com/iam/here/hereiam/MainActivity.java
+++ HereIam/app/src/main/java/com/iam/here/hereiam/MainActivity.java
@@ -3,6 +3,7 @@ package com.iam.here.hereiam;
 import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
@@ -23,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startLoginActivity(View view) {
+
+        Intent secondActivity = new Intent(this, LoginActivity.class);
+        startActivity(secondActivity);
     }
 
     /**

b83fe0bfc3562a8dfb7523cf542d5bf085dee67e
Eliezer de Jesus Oliveira <eliezerdejj@gmail.com>
2017-07-05 10:28:49 -0300

    Correções no Bug que fazia o app parar de funcionar

--- HereIam/app/src/main/java/com/iam/here/hereiam/cadastroAlunoTurma.java
+++ HereIam/app/src/main/java/com/iam/here/hereiam/cadastroAlunoTurma.java
@@ -3,16 +3,8 @@ package com.iam.here.hereiam;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-/**
- * Cadastra Aluno em uma turma existente
- */
 public class cadastroAlunoTurma extends AppCompatActivity {
 
-    /**
-     * Activity Lifecycle - Clico de vida da aplicação
-     * Cria a aplicação para ser visivel ao usuário
-     * @param savedInstanceState
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

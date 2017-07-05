e8847ed3d2fced2cb6d11948e6ea7f7dc522d4e9
Eliezer de Jesus Oliveira <eliezerdejj@gmail.com>
2017-07-04 22:46:11 -0300

    alterações

--- HereIam/app/src/main/java/com/iam/here/hereiam/cadastroAlunoTurma.java
+++ HereIam/app/src/main/java/com/iam/here/hereiam/cadastroAlunoTurma.java
@@ -16,7 +16,7 @@ public class cadastroAlunoTurma extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.cadastro_aluno_turma);
+        setContentView(R.layout.activity_cadastro_aluno_turma);
     }
 
     /**
